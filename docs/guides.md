481b9510f8b50cca724d4814bdf47df47595bc39
My Sou <mts398@humboldt.edu>
2019-09-27 01:26:57 -0700

    added code for keyboard input

--- docs/guides.md
+++ docs/guides.md
@@ -1,16 +1,21 @@
 # Helpful links and guides
 
 1. NCURSES Progromming HOWTO
+
 https://www.tldp.org/HOWTO/NCURSES-Programming-HOWTO/index.html
 
 2. Ncurses Programming Guide
+
 http://www.cs.ukzn.ac.za/~hughm/os/notes/ncurses.html
 
 3. Casual Coder's YouTube channel
+
 https://www.youtube.com/playlist?list=PL2U2TQ__OrQ8jTf0_noNKtHMuYlyxQl4v
 
 4. Basic Input/Output with Files
+
 http://www.cplusplus.com/doc/tutorial/files/
 
 5. Curses Library Functions
+
 https://docs.oracle.com/cd/E88353_01/html/E37849/makehtml-id-5.html#scrolltoc
--- projects/TextEditor/src/TextEdit/TextEdit/main.cpp
+++ projects/TextEditor/src/TextEdit/TextEdit/main.cpp
@@ -22,14 +22,12 @@ int main(int argc, char* argv[])
 	resize_term(num_rows - 1, num_cols - 1);
 	int window_x = getmaxyx(main_window, num_rows, num_cols);
 
-	//turn off key echo
-	noecho();
-	//nodelay(main_window, TRUE); //refreshes screen
-	keypad(main_window, TRUE); //turns on keypad
-	curs_set(0); //no blinking cursor
-	
-
-
+	//allows keyboard input 
+	nocbreak(); 
+	//turns on keypad
+	keypad(main_window, TRUE); 
+	//blinking cursor
+	curs_set(1); 
 
 	//===========MAIN_WINDOW BORDERS=========
 	for (int i = 0; i < num_cols; i++)
@@ -53,39 +51,29 @@ int main(int argc, char* argv[])
 	//tells curses to draw
 	refresh();
 
-	WINDOW* win = newwin(1, 1, 1, 1);
-	touchwin(win);
-	wrefresh(win);
-
 	//menu bar
-
 	WINDOW* menuWin = newwin(3, num_cols-2, 0, 1); //newwin(lines, nlines, columns, ncols)
 	box(menuWin, ACS_VLINE, ACS_HLINE); //small box window
 	mvwaddstr(menuWin, 1, 1, "File   Edit   Options   Tools   Help");
-
 	touchwin(menuWin);
 	wrefresh(menuWin);
-
 
 	//mapping out the status bar
 	WINDOW* statusWin = newwin(3, num_cols-2, 26, 1); //lines, width, vertical, horizontal
 	box(statusWin, ACS_VLINE, ACS_HLINE); //small box window
 	mvwaddstr(statusWin, 1, 1, "File type: .txt	|	Lines:	0	|	TextEditor");
-
 	touchwin(statusWin);
 	wrefresh(statusWin);
+
+	//mapping out the location of the line column counter
+	WINDOW* lineWin = newwin(num_rows - 8, 4, 3, 2); //lines, width, vertical, horizontal
+	mvwaddstr(lineWin, 1, 1, " 1\n  2\n  3\n  4\n  5\n  6\n  7\n  8\n  9\n 10\n 11\n 12\n 13\n 14\n 15\n 16");
+	touchwin(lineWin);
+	wrefresh(lineWin);
 
 	//text window
 	WINDOW* textWin = newwin(num_rows - 6, num_cols-6, 3, 5);
 	box(textWin, ACS_VLINE, ACS_HLINE);
-	
-
-	//mapping out the location of the line column counter
-	WINDOW* lineWin = newwin(num_rows-8, 4, 3, 2); //lines, width, vertical, horizontal
-	mvwaddstr(lineWin, 1, 1, " 1\n  2\n  3\n  4\n  5\n  6\n  7\n  8\n  9\n 10\n 11\n 12\n 13\n 14\n 15\n 16");
-
-	touchwin(lineWin);
-	wrefresh(lineWin);
 
 	//reading from hello.txt
 	string line;
@@ -94,39 +82,15 @@ int main(int argc, char* argv[])
 	{
 		while (getline(myfile, line))
 		{
-			mvwaddstr(textWin, 3, 5, line.c_str());
+			mvwaddstr(textWin, 1, 1, line.c_str());
 		}
 		myfile.close();
 	}
 	else cout << "Unable to open file";
 	touchwin(textWin);
 	wrefresh(textWin);
-	//mapping out the menu bar
-	//WINDOW* menuWin = newwin(3, num_cols-2, 0, 1); //newwin(lines, nlines, columns, ncols)
 
-	//box(menuWin, ACS_VLINE, ACS_HLINE); //small box window
-	//mvwaddstr(menuWin, 1, 1, "File   Edit   Options   Tools   Help");
-
-	//touchwin(menuWin);
-	//wrefresh(menuWin);
-	//
-	////mapping out the status bar
-	//WINDOW* statusWin = newwin(3, num_cols-2, 26, 1); //lines, width, vertical, horizontal
-	//box(statusWin, ACS_VLINE, ACS_HLINE); //small box window
-	//mvwaddstr(statusWin, 1, 1, "File type: .txt	|	Lines:	0	|	Text Editor Milestone 1: A Location Map, WOOHOO!!!!");
-
-	//touchwin(statusWin);
-	//wrefresh(statusWin);
-
-	////mapping out the location of the line column counter
-	//WINDOW* lineWin = newwin(num_rows-8, 4, 3, 2); //lines, width, vertical, horizontal
-	//box(lineWin, ' ', ' '); //small box window
-	//mvwaddstr(lineWin, 1, 1, " 1\n  2\n  3\n  4\n  5\n  6\n  7\n  8\n  9\n 10\n 11\n 12\n 13\n 14\n 15\n 16");
-
-	//touchwin(lineWin);
-	//wrefresh(lineWin);
-
-	//mvaddstr(4, 6, "  My first line of text.");
+	mvwgetch(textWin, 1, 1);
 
 	char result = getch();
 
